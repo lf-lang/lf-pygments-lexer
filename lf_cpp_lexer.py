3c4cc80bb1339cf5252457031bbcd42c56cdab81
Christian Menard <christian.menard@tu-dresden.de>
2023-01-11 17:34:23 +0100

    delegate everything within {= =} to the C++ lexer

--- lf_cpp_lexer.py
+++ lf_cpp_lexer.py
@@ -1,13 +1,21 @@
-from pygments.lexer import RegexLexer
+from pygments.lexer import DelegatingLexer, RegexLexer, bygroups
+from pygments.lexers.c_cpp import CppLexer
 from pygments.token import *
 
-class CustomLexer(RegexLexer):
+class CustomLexer(DelegatingLexer):
 
     name = 'LF-C++'
     filenames = ['*.lf']
 
+    def __init__(self, **options):
+        super().__init__(CppLexer, LFLexer, **options)
+
+
+
+class LFLexer(RegexLexer):
     tokens = {
         'root': [
-            (r'reactor', Keyword),
+            (r'({=)(\s*)(.*?)(\s*)(=})', bygroups(Punctuation, Whitespace, Other, Whitespace, Punctuation)),
+            (r'reactor', Keyword)
         ]
     }
